4e0a3135ad4f1d2f0bd6a7204edc0a2996136486
Arango0323 <161093065+Arango0323@users.noreply.github.com>
2024-04-12 17:12:51 -0500

    corrección etiquetas

--- parcial web/parcial.js
+++ parcial web/parcial.js
@@ -17,10 +17,10 @@ const printData = (data) => {
     let respuesta = document.getElementById('show_info')
     respuesta.innerHTML = `
     <h3 class ='info'>Name: ${data.name}</h3>
-    <p class = 'info'>Status: ${data.species}</p>
-    <p class = 'info'>Status: ${data.gender}</p>
+    <p class = 'info'>Species: ${data.species}</p>
+    <p class = 'info'>Gender: ${data.gender}</p>
     <p class = 'info'>Status: ${data.status}</p>
-    <p class = 'info'>Status: ${data.location.name}</p>
+    <p class = 'info'>Location: ${data.location.name}</p>
     <img src="${data.image}" alt="${data.name}">
 
     `
